b0b58b7c8af569f4a1c8af75f357326d43384e3d
Tran Ha <tranha.99884@gmail.com>
2023-07-31 15:21:17 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,30 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Todo from './views/Todo';
 
 const App = () => {
   let [name, setName] = useState('Hà')
   const [addrees, setAddrees] = useState('')
   const [todos, setTodos] = useState([
-    { id: 'todo1', title: 'Playing Game' },
-    { id: 'todo2', title: 'Home Word' },
-    { id: 'todo3', title: 'Fix Bus' }
-  ])
+    { id: 'todo1', title: 'Playing Game', type: 'Ha' },
+    { id: 'todo2', title: 'Home Word', type: 'Nhung' },
+    { id: 'todo3', title: 'Fix Bus', type: 'YEU' }
+  ]);
+
+  useEffect(() => {
+    console.log("run use effect");
+  }, [addrees]);
 
   const handldeOnClick = (event) => {
     if (!addrees) {
       alert("miss")
       return;
     }
-    let newTodo = { id: '', title: addrees }
+    let newTodo = { id: Math.floor(Math.random() * 101), title: addrees, type: '' }
     setTodos([...todos, newTodo])
     setAddrees('');
   }
   const handleOnChangeInput = (event) => {
     setAddrees(event.target.value)
 
+  }
+  const handldeDeleteTodo = (id) => {
+    let currentTodos = todos;
+    currentTodos = currentTodos.filter(item => item.id !== id)
+    setTodos(currentTodos)
   }
   return (
     <>
@@ -36,7 +45,13 @@ const App = () => {
           <h1>Xin Chào {name}</h1>
           <Todo
             todos={todos}
-
+            title={'All Title'}
+            handldeDeleteTodo={handldeDeleteTodo}
+          />
+          <Todo
+            todos={todos.filter(item => item.type === 'Ha')}
+            title={`Ha todo`}
+            handldeDeleteTodo={handldeDeleteTodo}
           />
           <input type='text' value={addrees} onChange={(event) => handleOnChangeInput(event)} />
           <button type='button'
--- src/views/Todo.js
+++ src/views/Todo.js
@@ -2,15 +2,29 @@
 
 const Todo = (props) => {
 
-    const todos = props.todos;
+    // const todos = props.todos;
+    const { todos, title, handldeDeleteTodo } = props;
+    const handldeDelet = (id) => {
+        handldeDeleteTodo(id)
+    }
     return (
         <div className='Todo-container'>
+            <div className="Title">
+                {title}
+            </div>
+
             {todos.map(todo => {
                 return (
-                    <li className='Todo-child' key={todo.id}>{todo.title}</li>
+                    <div key={todo.id}>
+                        <li className='Todo-child' >{todo.title}
+                            &nbsp;<span onClick={(id) => handldeDelet(todo.id)}>x</span>
+                        </li>
+                    </div>
                 )
             })}
+            <hr />
         </div>
+
     )
 }
 export default Todo;
